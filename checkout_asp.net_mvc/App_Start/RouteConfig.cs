90579203936e3a9ade2b97284602e1cb71b54a4f
Evgeni Kovalev <evgeni.kovalev@gmail.com>
2018-09-24 10:21:25 +0300

    Initial commit. Created new ASP .NET MVC project. I'm including also the project packages.

---
+++ checkout_asp.net_mvc/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace checkout_asp.net_mvc
+{
+  public class RouteConfig
+  {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="routes"></param>
+    public static void RegisterRoutes(RouteCollection routes)
+    {
+      routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+      routes.MapRoute(
+          name: "Default",
+          url: "{controller}/{action}/{id}",
+          defaults: new { controller = "Cart", action = "Index", id = UrlParameter.Optional }
+      );
+    }
+  }
+}
---
+++ checkout_asp.net_mvc/Controllers/CartController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace checkout_asp.net_mvc.Controllers
+{
+  public class CartController : Controller
+  {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
